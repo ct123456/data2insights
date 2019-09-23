ab70a52e24ff7cc5bab8a688bf4a76e1e6c07f77
Clifford Tham <ct123456@gmail.com>
2019-09-23 12:46:57 -0400

    Adding jobs to create subset data and to first job to split npi data into 2 separate entity types (hcp, hco)

---
+++ src/ingestion/split_nppes_by_entity_type.py
@@ -0,0 +1,25 @@
+from pyspark.sql import SparkSession, SQLContext
+import lib.utils as Utils
+from pyspark.sql.types import *
+
+
+if __name__ == "__main__":
+
+    spark = SparkSession.builder.appName("data2insights").getOrCreate()
+
+    sqlContext = SQLContext(spark)
+
+    s3_input_file_location = "s3a://data2insights/NPPES/parquet/npi_subset_10000"
+
+    s3_hcp_output_file_location = "s3a://data2insights/NPPES/parquet/npi_hcp_subset"
+    s3_hco_output_file_location = "s3a://data2insights/NPPES/parquet/npi_hco_subset"
+
+    npi_subset_df = sqlContext.read.parquet(s3_input_file_location)
+
+    npi_hcp_subset_df = npi_subset_df.where(npi_subset_df.entity_type_code == 1)
+    npi_hco_subset_df = npi_subset_df.where(npi_subset_df.entity_type_code == 2)
+
+    Utils.write_df_to_s3(npi_hcp_subset_df, s3_hcp_output_file_location)
+    Utils.write_df_to_s3(npi_hcp_subset_df, s3_hco_output_file_location)
+
+    spark.stop()
---
+++ src/ingestion/subset_medicare.py
@@ -0,0 +1,19 @@
+from pyspark.sql import SparkSession, SQLContext
+import lib.utils as Utils
+
+if __name__ == "__main__":
+
+    spark = SparkSession.builder.appName("data2insights").getOrCreate()
+
+    sqlContext = SQLContext(spark)
+
+    s3_input_file_location = "s3a://data2insights/Medicare/parquet/medicare_physicians_and_other_suppliers_2012_2015"
+    s3_output_file_location = (
+        "s3a://data2insights/Medicare/parquet/medicare_subset_10000"
+    )
+
+    medicare_df = sqlContext.read.parquet(s3_input_file_location)
+    medicare_subset_df = medicare_df.limit(10000)
+    Utils.write_df_to_s3(medicare_subset_df, s3_output_file_location)
+
+    spark.stop()
