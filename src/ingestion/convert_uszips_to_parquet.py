1297a8f75a016a8a4ab651e31bd9ec1254dd291e
Clifford Tham <ct123456@gmail.com>
2019-09-30 14:44:39 -0400

    Add dag for uszips info (zip to city, state, long, lat, etc)

---
+++ src/ingestion/convert_uszips_to_parquet.py
@@ -0,0 +1,19 @@
+from pyspark.sql import SparkSession, SQLContext
+import src.lib.utils as Utils
+from pyspark.sql.types import *
+
+
+if __name__ == "__main__":
+
+    spark = SparkSession.builder.appName("data2insights").getOrCreate()
+
+    sqlContext = SQLContext(spark)
+
+    s3_input_file_location = "s3a://data2insights/uszips/raw/*.csv"
+    s3_output_file_location = "s3a://data2insights/uszips/parquet/uszips"
+
+    Utils.convert_csvs_to_parquet(
+        spark, s3_input_file_location, s3_output_file_location
+    )
+
+    spark.stop()
---
+++ src/ingestion/write_uszips_to_db.py
@@ -0,0 +1,19 @@
+from pyspark.sql import SparkSession, SQLContext
+import pyspark.sql.functions as F
+from src.models.db_config import DbConfig
+import src.lib.db_utils as DbUtils
+
+if __name__ == "__main__":
+
+    spark = SparkSession.builder.appName("data2insights").getOrCreate()
+    sqlContext = SQLContext(spark)
+
+    db_config = DbConfig(db_name="healthcare", db_table="public.uszips")
+
+    s3_input_file_location = "s3a://data2insights/zipcode/parquet/uszips"
+
+    df = sqlContext.read.parquet(s3_input_file_location)
+
+    DbUtils.insert(db_config=db_config, dataframe=df)
+
+    spark.stop()
