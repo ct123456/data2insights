945ed976132892908defb8b140c2afb6e16bb6fc
Clifford Tham <ct123456@gmail.com>
2019-10-03 17:37:14 -0400

    Add zoom parameter to map

--- api/app.py
+++ api/app.py
@@ -26,7 +26,10 @@ def map_view():
     zipcode = request.args.get('zipcode', 10001)
     threshold = request.args.get('threshold', 1000)
 
-    return render_template('map_view.html', zipcode=zipcode, threshold=threshold)
+    zoom_level = 14 if zipcode else 5
+    zoom_level = request.args.get('zoom', zoom_level)
+
+    return render_template('map_view.html', zipcode=zipcode, threshold=threshold, zoom_level=zoom_level)
 
 
 @app.route('/app/provider/<npi>')
