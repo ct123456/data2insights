4f7b0b18c51a2d160be127357606c7db28616c10
Clifford Tham <ct123456@gmail.com>
2019-10-09 16:52:31 -0400

    Update institution repository

--- api/repositories/institution_repository.py
+++ api/repositories/institution_repository.py
@@ -6,15 +6,12 @@ class InstitutionRepository(object):
 
         query = """
         SELECT 
-            npi, 
-            entity_type_code, 
-            provider_organization_name_legal_business_name "name", 
-            provider_first_line_business_mailing_address "address1", 
-            provider_second_line_business_mailing_address "address2", 
-            provider_business_mailing_address_city_name "city", 
-            provider_business_mailing_address_state_name "state", 
-            zip5 "zip_code"
-        FROM npi_hco hco
+            "npi", 
+            "name", 
+            "medicare_count",
+            "score",
+            "zip_code"
+        FROM institution hco
         WHERE hco.npi='{npi}'
         """.format(
             npi=npi
@@ -28,13 +25,10 @@ class InstitutionRepository(object):
             items.append(
                 {
                     "npi": row[0],
-                    "entity_type_code": row[1],
-                    "name": row[2],
-                    "address1": row[3],
-                    "address2": row[4],
-                    "city": row[5],
-                    "state": row[6],
-                    "zip_code": row[7]
+                    "name": row[1],
+                    "medicare_count": row[2],
+                    "score": row[3],
+                    "zip_code": row[4]
                 }
             )
 
@@ -43,16 +37,13 @@ class InstitutionRepository(object):
     def get_by_zipcode(self, zipcode, limit=100, offset=0):
         query = """
         SELECT 
-            npi, 
-            entity_type_code, 
-            provider_organization_name_legal_business_name "name", 
-            provider_first_line_business_mailing_address "address1", 
-            provider_second_line_business_mailing_address "address2", 
-            provider_business_mailing_address_city_name "city", 
-            provider_business_mailing_address_state_name "state", 
-            zip5 "zip_code"
-        FROM npi_hco hco
-        WHERE hco.zip5='{zipcode}'
+            "npi", 
+            "name", 
+            "medicare_count",
+            "score",
+            "zip_code"
+        FROM institution hco
+        WHERE hco.zip_code='{zipcode}'
         ORDER BY npi DESC
         LIMIT {limit}
         OFFSET {offset}
@@ -70,13 +61,10 @@ class InstitutionRepository(object):
             items.append(
                 {
                     "npi": row[0],
-                    "entity_type_code": row[1],
-                    "name": row[2],
-                    "address1": row[3],
-                    "address2": row[4],
-                    "city": row[5],
-                    "state": row[6],
-                    "zip_code": row[7]
+                    "name": row[1],
+                    "medicare_count": row[2],
+                    "score": row[3],
+                    "zip_code": row[4]
                 }
             )
 
